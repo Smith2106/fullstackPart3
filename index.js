5e313f4068069d887c8edf87ea7e18a1a53e081b
Elijah <elijahs2106@gmail.com>
2019-07-19 00:49:22 -0400

    Finish exercises 3.7 and 3.8

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+
+morgan.token('content', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 let persons = [
   {
