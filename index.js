3756548b7fc97bbc6713669ea295f350a3fb8ccf
Elijah <elijahs2106@gmail.com>
2019-07-19 14:08:44 -0400

    Add Procfile for Heroku

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors())
 
 morgan.token('content', (req, res) => {
   return JSON.stringify(req.body)
@@ -94,7 +96,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`)
 })
